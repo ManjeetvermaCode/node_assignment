f0f5c962615ce20c4075e283a7210a6020663816
manjeetverma <manjeetverma@gmail.com>
2023-12-01 22:41:26 +0530

    done with assignment1

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.get('/api/',async(req,res)=>{//This route gets 'data' from all the pages.
     }
     const allData='received_Data/all_data_response.txt'
     fs.writeFileSync(allData,JSON.stringify(arr,null,2))// Log the response AS-IS to a text file
-    res.json(arr)
+    res.status(201).json(arr)
    }catch(error){
-    res.send(`Something went wrong, ${error}`)
+    res.status(400).send(`Something went wrong, ${error}`)
    }
 })
 
@@ -33,22 +33,22 @@ app.get('/api/:pg',async(req,res)=>{// This route gets data from a specific page
         const file=`received_Data/pg${pg}.txt`
         const {data}=await axios.get(`${Url}?page=${pg}`)
         fs.writeFileSync(file,JSON.stringify(data,null,2))// Log the response AS-IS to a text file
-        res.send(data)
+        res.status(200).send(data)
     }catch(error){
-        res.send(`Unable to fetch data from page number ${pg}`)
+        res.status(400).send(`Unable to fetch data from page number ${pg}`)
     }
 })
 
-app.get('/api/country/:pg',async(req,res)=>{//This route return cat breeds grouped by Country 
+app.get('/api/country/:pg',async(req,res)=>{//This route returns cat breeds grouped by Country 
     try{
         const {pg}=req.params
         const {data}=await axios.get(`${Url}?page=${pg}`)
         const result=groupBy(data.data,'country')
         const file=`received_Data/groupByCountry_${pg}.txt`
         fs.writeFileSync(file,JSON.stringify(result,null,2))// Log the response AS-IS to a text file
-        res.json(result)
+        res.status(200).json(result)
     }catch(error){
-        res.send('Unable to return cat breeds grouped by Country- ', error)
+        res.status(400).send('Unable to return cat breeds grouped by Country- ', error)
     }
 
 })
