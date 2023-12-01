3902705ae88b695db2f2b723f8d49e645cefe08b
manjeetverma <manjeetverma@gmail.com>
2023-12-01 16:43:13 +0530

    wrote route for returning all data from all pages and route that displays data of a specific page.

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express=require('express')
+const axios=require('axios')
+
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.send('home page')
+//middlewares
+app.use(express.json())
+
+
+const Url='https://catfact.ninja/breeds'
+
+
+app.get('/',async(req,res)=>{
+   try{
+    const arr=[]
+    for(let i=0;i<5;i++){
+        const {data}=await axios.get(`${Url}?page=${i+1}`)
+        arr.push(data)
+    }
+    res.json(arr)
+   }catch(error){
+    res.send(`Something went wrong, ${error}`)
+   }
+})
+
+app.get('/:pg',async(req,res)=>{
+    try{
+        const {pg}=req.params
+        const {data}=await axios.get(`${Url}?page=${pg}`)
+        res.send(data)
+    }catch(error){
+        res.send(`Unable to fetch data from page number ${pg}`)
+    }
 })
 
 app.listen('3000',()=>{
